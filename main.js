07868fd21b5d4fe262cc3c36470b36698865a352
Rudraanshagarwal <88777342+Rudraanshagarwal@users.noreply.github.com>
2023-05-25 14:50:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+function draw(){
+    background("green");
+}
+function setup(){
+canvas=createCanvas(400,300);
+camera=createCapture(VIDEO);
+camera.position(100,250);
+camera.size(400,300);
+canvas.position(600,250);
+}
+
+function preload(){
+
+}
